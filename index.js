e16b56e8aeec56b60c1086d4729ecb3057852b6a
NewBeez001 <aldrinpads92@gmail.com>
2023-06-13 15:47:25 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const toggleButton = document.getElementsByClassName('toggle-button')[0]
+    const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+    toggleButton.addEventListener('click', () => {
+        navbarLinks.classList.toggle('active');
+        
+    });
+})
